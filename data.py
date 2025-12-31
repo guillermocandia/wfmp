8aff5b8d70ffc0892cce52cfd0df8821478911a5
Guillermo Candia Huerta <guillermo.candia@gmail.com>
2025-12-31 01:42:09 -0300

    fix data

--- data.py
+++ data.py
@@ -27,8 +27,39 @@ def get_items():
         except Exception:
             pass
 
-    data = data.get("data", [])
+    return data.get("data", None)
 
+
+@st.cache_data(persist=True)
+def get_item_detail(item):
+    conn = http.client.HTTPSConnection("api.warframe.market")
+    headers = {"platform": "pc", "crossplay": "true"}
+    slug = item.get("slug", None)
+
+    if slug is None:
+        return None
+
+    try:
+        conn.request("GET", f"/v2/orders/item/{slug}", headers=headers)
+        response = conn.getresponse()
+        if response.status != 200:
+            return None
+        data = response.read().decode("utf-8")
+        data = json.loads(data)
+    except Exception:
+        return None
+    finally:
+        try:
+            conn.close()
+        except Exception:
+            pass
+
+    time.sleep(0.35)
+
+    return data.get("data", None)
+
+
+def process_items(data):
     filtered_items = [
         x
         for x in data
@@ -62,59 +93,56 @@ def get_items():
     return items
 
 
-@st.cache_data(persist=True)
-def get_item_detail(item):
-    conn = http.client.HTTPSConnection("api.warframe.market")
-    headers = {"platform": "pc", "crossplay": "true"}
-    slug = item.get("slug", None)
+def process_items_detail(data, item):
+    if data is None:
+        return {
+            "min_price_rank_0": None,
+            "min_price_max_rank": None,
+            "average_price_rank_0": None,
+            "average_price_max_rank": None,
+        }
+    sell_orders = [x for x in data if x.get("type", None) == "sell"]
 
-    if slug is None:
-        return {"min_price": None, "average_price": None}
-    try:
-        conn.request("GET", f"/v2/orders/item/{slug}", headers=headers)
-        response = conn.getresponse()
-        if response.status != 200:
-            return {"min_price": None, "average_price": None}
-        data = response.read().decode("utf-8")
-        data = json.loads(data)
-    except Exception:
-        return {"min_price": None, "average_price": None}
-    finally:
-        try:
-            conn.close()
-        except Exception:
-            pass
+    sell_orders_rank_0 = [x.get("platinum") for x in sell_orders if x.get("rank") == 0]
 
-    data = data.get("data", [])
-
-    sell_orders = [
-        x.get("platinum")
-        for x in data
-        if x.get("type", None) == "sell" and x.get("rank") == item.get("max_rank")
-        # FIX: only use max rank for arcanes
-        # or extract data for unranked and maxed
+    sell_orders_max_rank = [
+        x.get("platinum") for x in sell_orders if x.get("rank") == item.get("max_rank")
     ]
 
-    time.sleep(0.35)
-    return {"min_price": min(sell_orders), "average_price": average(sell_orders)}
+    return {
+        "min_price_rank_0": (
+            min(sell_orders_rank_0) if len(sell_orders_rank_0) > 0 else None
+        ),
+        "min_price_max_rank": (
+            min(sell_orders_max_rank) if len(sell_orders_max_rank) > 0 else None
+        ),
+        "average_price_rank_0": (
+            average(sell_orders_rank_0) if len(sell_orders_rank_0) > 0 else None
+        ),
+        "average_price_max_rank": (
+            average(sell_orders_max_rank) if len(sell_orders_max_rank) > 0 else None
+        ),
+    }
 
 
-@st.cache_data(persist=True)
 def load_data():
-    items = get_items()
-    if not items:
+    raw_items = get_items()
+    if not raw_items:
         return pd.DataFrame(), pd.DataFrame()
+
+    items = process_items(raw_items)
 
     count = 0
     total = len(items)
-    items_with_price = []
-    for x in items:
-        details = get_item_detail(x)
-        items_with_price.append(x | details)
+    items_with_details = []
+    for item in items:
+        raw_items_details = get_item_detail(item)
+        item_details = process_items_detail(raw_items_details, item)
+        items_with_details.append(item | item_details)
         count += 1
         print(f"{count}/{total}")
 
-    df = pd.DataFrame(items_with_price)
+    df = pd.DataFrame(items_with_details)
 
     arcanes_df = df[df["type"] == "arcane"].reset_index(drop=True)
     mods_df = df[df["type"] == "mod"].reset_index(drop=True)
